aa830d3e0919440fe23b499f725f6f6f663c69d7
cynicade <cynkvs@gmail.com>
2022-06-11 20:46:32 +0200

    changed grid request message

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -5,7 +5,7 @@ export const io = new Server({ cors: { origin: "*" } });
 
 function handleConnection(socket: Socket): void {
   console.log(socket.id);
-  socket.on("player_joined", (diff) => {
+  socket.on("request_grid", (diff) => {
     socket.emit("new_grid", makeGrid(diff));
   });
   socket.on("disconnect", (reason) => {
